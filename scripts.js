3de5dba46f8e381c154499b6f0433647859c2b73
Wojciech Szczyrba <benice195@gmail.com>
2017-01-08 20:33:43 +0100

    kanban do zmiany

--- scripts.js
+++ scripts.js
@@ -9,6 +9,7 @@ $(function() {
         };
         return str;
     };
+
     function Column(name) {
         var self = this; // przyda się dla funkcji zagnieżdżonych
 
@@ -28,9 +29,81 @@ $(function() {
             });
             //Dodawanie karteczki po kliknięciu w przycisk:
             $columnAddCard.click(function() {
-                    self.addCard(new Card(prompt("Wpisz nazwę karty")));
+                self.addCard(new Card(prompt("Wpisz nazwę karty")));
+                this.$element.children('ul').append(card.$element);
             });
+            $column.append($columnTitle)
+                    .append($columnDelete)
+                    .append($columnAddCard)
+                    .append($columnCardList);
+            return $column;
         };
+    };
         //PODPINANIE ZDARZEN
-  }
-})
+   Column.prototype = {
+        addCard: function(card) {
+        this.$element.children('ul').append(card.$element);
+        },
+        removeColumn: function() {
+        this.$element.remove();
+        }
+    };
+    function Card(description) {
+        var self = this;
+
+        this.id = randomString();
+        this.description = description;
+        this.$element = createCard(); //
+
+        function createCard() {
+            var $card = $('<li>').addClass('card');
+            var $cardDescription = $('<p>').addClass('card-description').text(self.description);
+            var $cardDelete = $('<button>').addClass('btn-delete').text('x');
+            $cardDelete.click(function(){
+                self.removeCard();
+            });
+            $card.append($cardDelete)
+                .append($cardDescription);
+            return $card;
+        };
+
+    };
+    var board = {
+        name: 'Tablica Kanban',
+        addColumn: function(column) {
+        this.$element.append(column.$element);
+        initSortable();
+        },
+        $element: $('#board .column-container')
+    };
+    function initSortable() {
+        $('.column-card-list').sortable({
+        connectWith: '.column-card-list',
+        placeholder: 'card-placeholder'
+        }).disableSelection();
+    }
+    console.log('dziala');
+    $('.create-column')
+        .click(function(){
+            var name = prompt('Wpisz nazwę kolumny');
+            var column = new Column(name);
+                board.addColumn(column);
+        });
+    // TWORZENIE KOLUMN
+    var todoColumn = new Column('Do zrobienia');
+    var doingColumn = new Column('W trakcie');
+    var doneColumn = new Column('Skończone');
+
+    // DODAWANIE KOLUMN DO TABLICY
+    board.addColumn(todoColumn);
+    board.addColumn(doingColumn);
+    board.addColumn(doneColumn);
+
+    // TWORZENIE NOWYCH EGZEMPLARZY KART
+    var card1 = new Card('Nowe zadanie');
+    var card2 = new Card('Stworzyc tablice kanban');
+
+    // DODAWANIE KART DO KOLUMN
+    todoColumn.addCard(card1);
+    doingColumn.addCard(card2);
+});
